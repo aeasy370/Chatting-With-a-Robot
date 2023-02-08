e9183ee08eae845e2a583a19eff964ba733c7379
rs1856tn <81371924+rs1856tn@users.noreply.github.com>
2023-02-08 12:59:59 -0500

    h

--- socket_s.py
+++ socket_s.py
@@ -5,6 +5,8 @@ import socketserver
 TCP_IP = 'localhost'
 TCP_PORT = 9001
 BUFFER_SIZE = 1024
+
+
 class ClientThread(Thread):
     def __init__(self, ip, port, sock):
         Thread.__init__(self)
@@ -12,34 +14,21 @@ class ClientThread(Thread):
         self.port = port
         self.sock = sock
         print("new thread " + ip +":" +str(port))
-
-    def run(self):
+        
+    def send(self):
         filename = input("input file name")
         f = open(filename, 'rb')
         while True:
             l = f.read(BUFFER_SIZE)
             while (l):
                 self.sock.send(l)
+                #print('Sent ',repr(l))
                 l = f.read(BUFFER_SIZE)
             if not l:
                 f.close()
                 #self.sock.close()
                 break
-
-
-tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-tcpsock.bind((TCP_IP, TCP_PORT))
-threads = []
             
-while True:
-    tcpsock.listen(5)
-    print("Waiting for incoming connections...")
-    (conn, (ip,port)) = tcpsock.accept()
-    print('Got connection from ', (ip,port))
-    newthread = ClientThread(ip,port,conn)
-    newthread.start()
-    threads.append(newthread)
-
-for t in threads:
-    t.join()
+    def recieve(self):
+        
+        
