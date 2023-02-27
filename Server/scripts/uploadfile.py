d2101aaa2f2e9d0ff745991a6e9519352e378b49
Matt Wyatt <mwyatt1000@gmail.com>
2023-02-27 16:44:22 -0500

    ensure servaudiofiles directory exists on startup

---
+++ Server/scripts/uploadfile.py
@@ -0,0 +1,12 @@
+from sys import argv, stderr
+import requests
+
+if __name__ == "__main__":
+  if len(argv) < 2:
+    print("usage: uploadfile.py <audio_name.mp3>", file=stderr)
+    raise SystemExit
+  
+  f = open(f"../audio/{argv[1]}", "rb")
+  r = requests.post("http://localhost:5000", files={"file": f})
+  print(r)
+  print(r.text)
