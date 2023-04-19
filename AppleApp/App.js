df3bbc2da2c931c9c424cfda0f5bad1827c7370b
Michael Hughes <mhughe36@vols.utk.edu>
2023-04-19 13:57:24 -0400

    apple app update

--- AppleApp/App.js
+++ AppleApp/App.js
@@ -1,8 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Audio } from 'expo-av';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TextInput } from 'react-native';
 import * as Sharing from 'expo-sharing';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
   const [recording, setRecording] = React.useState();
@@ -68,26 +71,120 @@ export default function App() {
   }
 
 
-  return (
-    
-    <View style={styles.container}>
-      <Text>{message}</Text>
-      {/*Wrapper for buttons and title*/}
-      <View style={styles.buttonWrapper}>
-        <Text style={styles.sectionTitle}>Hit a button to record or to send to server!</Text>
-        {/*Button for Voice Recording*/}
-        <Button
-          title={recording ? 'Stop Recording' : 'Start Recording'}
-          onPress={recording ? stopRecording : startRecording}
-        />
-        {getRecordingLines()}
-        <Button
-          title="Send to Server"
-        />
-      </View>
-      
-      <StatusBar style="auto" />
+  function SettingsScreen ({navigation}){
+    /* Declaration of Settings Screen */
+    const saveServer = async() => {
+      /* Saving server name */ 
+      try {
+        AsyncStorage.setItem('Server', JSON.stringify(text));
+      } catch (error) {
+        console.log(error);
+        alert('Error with Saving Server');
+      }
+    };
+  
+    const getServer = async() => {
+      /* Get Server name from save */
+      try {
+        const servname = JSON.parse(await AsyncStorage.getItem('Server'));
+        onChangeText(servname);
+      } catch (error) {
+        console.log(error);
+        alert('Error with Loading Server');
+      }
+    };
+    const savePort = async() => {
+      /* Save Port Number */
+      try {
+        AsyncStorage.setItem('Port', JSON.stringify(number));
+      } catch (error) {
+        console.log(error);
+        alert('Error with Saving Port');
+      }
+    };
+  
+    const getPort = async() => {
+      /* Get Port Number From Save */
+      try {
+        const portname = JSON.parse(await AsyncStorage.getItem('Port'));
+        onChangeNumber(portname);
+      } catch (error) {
+        console.log(error);
+        alert('Error with Loading Port');
+      }
+    };
+
+    React.useEffect(() => {
+      /*Get Server and Port on Boot up */
+      getServer();
+      getPort();
+    }, []);
+    const [text, onChangeText] = React.useState('');
+    const [number, onChangeNumber] = React.useState('');
+    return(
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        placeholder='Server'
+        value={text}
+      />
+      <Button
+      title="Save server name"
+      onPress={saveServer}
+      />
+     <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="Port"
+        keyboardType="numeric"
+      />
+      <Button
+      title="Save Port Number"
+      onPress={savePort}
+      />
+      <Button
+      title="Back to Home"
+      onPress={() => navigation.navigate('Home')}
+      />
     </View>
+    );
+  }
+  function HomeScreen ({navigation}) {
+    /* Declaration of Home Screen */
+    return (
+      <View style={styles.container}>
+        <Text>{message}</Text>
+        {/*Wrapper for buttons and title*/}
+        <View style={styles.buttonWrapper}>
+          <Text style={styles.sectionTitle}>Hit a button to record or to send to server!</Text>
+          {/*Button for Voice Recording*/}
+          <Button
+            title={recording ? 'Stop Recording' : 'Start Recording'}
+            onPress={recording ? stopRecording : startRecording}
+          />
+          {getRecordingLines()}
+          <Button
+            title="Send to Server"
+          />
+          <Button
+          title="Go to Settings"
+          onPress={() => navigation.navigate('Settings')}
+          />
+        </View>
+        <StatusBar style="light" />
+      </View>
+    );
+  }
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} /> 
+      <Stack.Screen name="Settings" component={SettingsScreen} />   
+      </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
