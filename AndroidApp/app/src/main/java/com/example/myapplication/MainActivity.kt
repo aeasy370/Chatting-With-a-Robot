b6076e9a95d7570d84ef8b7e7f4863a4c8bc5059
gregbro2 <73258266+gregbro2@users.noreply.github.com>
2023-05-02 00:59:24 -0400

    Finished base app functionality

--- AndroidApp/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ AndroidApp/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import android.media.MediaRecorder
@@ -10,14 +11,13 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
@@ -47,6 +47,7 @@ class MainActivity : ComponentActivity() {
     private var output: String? = null
     private var outputFile: File? = null
 
+
     fun startRecording(mediaRecorder: MediaRecorder, output: String){
         Log.d("Testing output", output)
         mediaRecorder?.prepare()
@@ -55,17 +56,16 @@ class MainActivity : ComponentActivity() {
     fun stopRecording(mediaRecorder: MediaRecorder){
         mediaRecorder?.stop()
         mediaRecorder?.release()
-        mediaRecorder?.reset()
 
     }
     @RequiresApi(Build.VERSION_CODES.Q)
     fun sendRecording(recordingFile: File){
         val okHttpClient = OkHttpClient.Builder()
-            .readTimeout(5, TimeUnit.MINUTES)
-            .connectTimeout(5, TimeUnit.MINUTES)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS)
             .build()
         val retrofitBuilder = Retrofit.Builder()
-            .baseUrl("http://10.131.212.122:5301")
+            .baseUrl("http://216.96.192.192:25623/ ")
             .client(okHttpClient)
             .build()
         val recordingRequestBody = MultipartBody.Builder().setType(MultipartBody.FORM)
@@ -76,14 +76,31 @@ class MainActivity : ComponentActivity() {
         val recordingService = retrofitBuilder.create(AudioService::class.java)
         //val filePartBody = MultipartBody.Part.createFormData("file", recordingFile.name, recordingFile.asRequestBody())
         runBlocking {
-            launch { recordingService.sendAudio(recordingRequestBody)
-            recordingService.recieveData(recordingFile.name)}
+            launch { recordingService.sendAudio(recordingRequestBody) }
         }
 
 
     }
 
-    @RequiresApi(Build.VERSION_CODES.S)
+    fun recieveRecording(oFile: File, text: MutableState<String>){
+        val okHttpClient = OkHttpClient.Builder()
+            .readTimeout(30, TimeUnit.SECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .build()
+        val retrofitBuilder = Retrofit.Builder()
+            .baseUrl("http://216.96.192.192:25623/ ")
+            .client(okHttpClient)
+            .build()
+
+        val recordingService = retrofitBuilder.create(AudioService::class.java)
+        runBlocking {
+            launch {  val response = recordingService.recieveData(oFile.name)
+            text.value = response.string()}
+        }
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (ContextCompat.checkSelfPermission(this,
@@ -92,7 +109,8 @@ class MainActivity : ComponentActivity() {
             val permissions = arrayOf(Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
             ActivityCompat.requestPermissions(this, permissions,0)
         }
-        mediaRecorder = MediaRecorder(applicationContext)
+        val text = mutableStateOf("text")
+        mediaRecorder = MediaRecorder()
         output = applicationContext.getExternalFilesDir(null)!!.absolutePath + "/recording.3gp"
         outputFile = File(output)
         outputFile!!.createNewFile()
@@ -100,13 +118,18 @@ class MainActivity : ComponentActivity() {
         mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
         mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
         mediaRecorder?.setOutputFile(outputFile)
+
         setContent {
             MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier, color = MaterialTheme.colors.background) {
+
+                Surface(modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth(), color = Color(0xFFFFFFF)) {
                     SpeechTest(startonClick = {startRecording(mediaRecorder!!, output!!)},
                         stoponClick = {stopRecording(mediaRecorder!!)},
-                        sendOnClick = {sendRecording(outputFile!!)}
+                        sendOnClick = {sendRecording(outputFile!!)},
+                        recieveOnClick = {recieveRecording(outputFile!!, text)},
+                        text = text
                     )
                 }
             }
@@ -114,27 +137,41 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
-fun SpeechTest(startonClick: () -> Unit, stoponClick: () -> Unit, sendOnClick: () -> Unit) {
-Column() {
-    Button(onClick = startonClick) {
-        Text("Start Recording")
+fun SpeechTest(startonClick: () -> Unit, stoponClick: () -> Unit, sendOnClick: () -> Unit, recieveOnClick: () -> Unit, text: MutableState<String>){
+
+    Column {
+    Button(onClick = startonClick,colors = ButtonDefaults.outlinedButtonColors(backgroundColor = Color(0xFFFF8200))) {
+        Text("Start Recording", color = Color.White)
 
     }
-    Button(onClick = stoponClick) {
-        Text("Stop Recording")
+    Button(onClick = stoponClick, colors = ButtonDefaults.outlinedButtonColors(backgroundColor = Color(0xFFFF8200))) {
+        Text("Stop Recording", color = Color.White)
 
     }
-    Button(onClick = sendOnClick){
-        Text("Send Recording to Server")
+    Button(onClick = sendOnClick, colors = ButtonDefaults.outlinedButtonColors(backgroundColor = Color(0xFFFF8200))){
+        Text("Send Recording to Server", color = Color.White)
     }
+        Button(onClick = recieveOnClick, colors = ButtonDefaults.outlinedButtonColors(backgroundColor = Color(0xFFFF8200))){
+            Text("Receive Data", color = Color.White)
+        }
+    Spacer(modifier = Modifier.height(20.dp))
+    Text(text.value)
+    Spacer(modifier = Modifier.height(20.dp))
+    Button(onClick = {}, colors = ButtonDefaults.outlinedButtonColors(backgroundColor = Color.White)){
+        Text("Settings", color = Color(0xFFFF8200))
+    }
+
 }
 }
 
+@SuppressLint("UnrememberedMutableState")
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
+    val text = mutableStateOf("text")
     MyApplicationTheme {
-        SpeechTest({},{},{})
+        SpeechTest({},{},{},{},text)
     }
 }
